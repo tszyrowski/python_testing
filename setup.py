f2f1805ff938ef69e3013ae4ec69f3b09e4a7e8d
tszyrowski <tszyrowski@gmail.com>
2019-07-20 12:37:55 +0100

    update gitignore with distribution packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='python_testing_workshoop',
+    name='python_testing_workshop',
     version='0.1dev',
     packages=['basic_pytest',],
     licence='MIT License',
