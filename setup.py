3a11f5f78ad064951072c4554ddd4a1f9c61f7e9
tszyrowski <tszyrowski@gmail.com>
2019-07-20 12:26:11 +0100

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='python_testing_workshoop',
+    version='0.1dev',
+    packages=['basic_pytest',],
+    licence='MIT License',
+    long_description=open('README.md').read(),
+)
